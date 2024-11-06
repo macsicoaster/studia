6a909e32efa21c267ffeda410e3ed1206f3af705
macsicoaster <maciejklossek@macbook-air-maciej-1.home>
2024-11-06 09:13:18 +0100

    dodano extensions

---
+++ sem5/geodane/extensions.sql
@@ -0,0 +1,9 @@
+
+CREATE EXTENSION postgis;
+CREATE EXTENSION postgis_sfcgal;
+CREATE EXTENSION postgis_raster;
+CREATE EXTENSION fuzzystrmatch;
+CREATE EXTENSION address_standardizer;
+CREATE EXTENSION address_standardizer_data_us;
+CREATE EXTENSION postgis_tiger_geocoder;
+CREATE EXTENSION postgis_topology;
