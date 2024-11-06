d8ecab52760b803a56abec905007f3f8e71218f2
macsicoaster <retromaciek@op.pl>
2024-11-06 19:07:55 +0100

    geodane po labach, naprawic to, ze sie sql nie dodaje do bazy danych

---
+++ sem5/geodane/zadaniapostgisgridtbd.sql
@@ -0,0 +1,49 @@
+--Zadanie
+--1. Oblicz wysokość centroidu dzielnicy
+--select dziel.gid ids, grd.z_mean z from gran_dzielnice dziel inner join grd_200m grd on ST_Intersects(ST_Force2D(ST_Centroid(dziel.geom)),ST_Force2D(grd.geom))
+--select dziel.gid ids, grd.z_mean z from gran_dzielnice dziel 
+--inner join grd_200m grd on ST_Intersects(ST_Force2D(ST_Centroid(dziel.geom)),ST_Force2D(grd.geom));
+
+DO
+$$
+declare
+ids integer;
+z decimal(10,3);
+begin
+alter table gran_dzielnice add column if not exists Hn decimal(10,3);
+for ids, z in (select dziel.gid ids, grd.z_mean z from gran_dzielnice dziel inner join grd_200m grd
+on ST_Intersects(ST_Force2D(ST_Centroid(dziel.geom)),ST_Force2D(grd.geom)))
+loop
+execute format ('update gran_dzielnice set Hn=%s where gid=%s',z,ids);
+--RAISE NOTICE USING MESSAGE=CAST(z as varchar(20));
+end loop;
+end;
+$$ LANGUAGE plpgsql;
+
+--ST_Force2D stosować dla geometrii 3D !!!!!
+
+--2. Oblicz średnią wysokość w dzielnicy
+select dziel.gid ids, AVG(grd.z_mean) z from gran_dzielnice dziel inner join grd_200m grd
+on ST_Intersects(ST_Force2D(dziel.geom),ST_Force2D(grd.geom))
+group by ids;
+
+
+--3. Oblicz liczbę budynków w dzielnicach
+select dziel.gid ids, bud.gid idb from gran_dzielnice dziel inner join budynki bud
+on ST_Intersects(ST_Force2D(dziel.geom),ST_Force2D(bud.geom));
+
+
+--4. Oblicz średnią powierzchnię (zabudowy, po obrysie) budynków w dzielnicach
+select dziel.gid ids, AVG(ST_Area(bud.geom)) mean_pow from gran_dzielnice dziel inner join budynki bud 
+on ST_Intersects(ST_Force2D(dziel.geom),ST_Force2D(bud.geom))
+group by ids;
+
+
+--5. Oblicz średnią wysokość w otoczeniu 200 m od budynków
+select bud.gid ids, AVG(grd.z_mean) z_mean from budynki bud inner join grd_200m grd
+on ST_Intersects(ST_Buffer(bud.geom,200,8), grd.geom)
+group by ids;
+
+--6. Oblicz liczbę otworów hydrogeologicznych w siatce grd_200m
+
+
