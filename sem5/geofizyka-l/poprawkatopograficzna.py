2fc12211140c4c6961d8458552262fa6c19f4589
macsicoaster <retromaciek@op.pl>
2024-11-10 14:10:55 +0100

    added

---
+++ sem5/geofizyka-l/poprawkatopograficzna.py
@@ -0,0 +1,58 @@
+# Importujemy potrzebne biblioteki
+import math
+import numpy as np
+import geopandas as gpd
+from geopandas.geodataframe import GeoDataFrame
+
+# Ścieżki do plików SHP
+nmt5_path = 'nmt5.shp'
+dane5_path = 'dane5.shp'
+
+# Wczytanie plików SHP
+nmt5_gpd = gpd.read_file(nmt5_path)
+dane5_gpd = gpd.read_file(dane5_path)
+
+# Upewnij się, że oba zbiory danych mają ten sam układ współrzędnych (CRS)
+nmt5_gpd = nmt5_gpd.to_crs(dane5_gpd.crs)
+
+# Ustawienie promienia dla bufora
+radius = 1200  # jednostki zgodne z CRS, np. w metrach
+
+# Utworzenie bufora wokół punktów w dane5
+dane5_gpd['buffer'] = dane5_gpd.geometry.buffer(radius)
+
+# Zmieniamy kolumnę 'buffer' na nową geometrię
+dane5_gpd = dane5_gpd.set_geometry('buffer')
+
+# Przestrzenne połączenie, aby znaleźć punkty nmt5 w obrębie bufora punktów dane5
+# Wykorzystujemy 'sjoin' z predykatem 'within'
+nmt5_in_radius = gpd.sjoin(nmt5_gpd, dane5_gpd, predicate='within', how='inner')
+
+# Wynikowy GeoDataFrame `nmt5_in_radius` zawiera teraz punkty nmt5 znajdujące się w promieniu `radius` od każdego punktu w `dane5`.
+# Możesz wyczyścić `buffer` z `dane5_gpd` jeśli jest już zbędny
+dane5_gpd: GeoDataFrame = dane5_gpd.drop(columns=['buffer'])
+
+
+centers = []
+for row in dane5_gpd.T:
+    data = dane5_gpd.iloc[row]
+    ng = data['NG']
+    eg = data['EG']
+    centers.append((ng,eg))
+
+center_points = {k: [] for k in centers}
+
+for center, points in center_points:
+    
+
+    
+
+
+
+srodki = [
+
+
+
+
+
+]
